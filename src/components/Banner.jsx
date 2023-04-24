65dfb908ee29074544803e88d3438575c286b88c
romel <romelanthonylopez@gmail.com>
2023-04-24 16:49:22 -0700

    Laptop test run

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
 
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Salsa!", "Bachata!", "Circuit Training!", "Kickboxing!", "Zumba!"]
+    const toRotate = ["Salsa!", "Bachata!", "Circuit Training", "Kickboxing!", "Zumba!"]
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 900;
